a60f7fc4341256d7bdb27f9c9c59f5b5fa48249a
soren vinson <toodarkpark1818@gmail.com>
2026-03-27 13:31:12 -0700

    feat: add AdminMetricsStrip and AdminTimelineDivider dumb components

---
+++ src/components/dumb/admin/AdminTimelineDivider.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  label: string
+}>()
+</script>
+
+<template>
+  <div class="text-caption text-medium-emphasis text-uppercase font-weight-medium mt-3 mb-2 pb-1" style="letter-spacing: 0.5px; border-bottom: 1px solid rgb(var(--v-border-color));">
+    {{ label }}
+  </div>
+</template>
