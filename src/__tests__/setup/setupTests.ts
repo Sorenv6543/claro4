f1393059cb9dcf35ed3c3b4ebff9c8ff69ead8bd
soren vinson <127169480+Sorenv6543@users.noreply.github.com>
2026-03-27 19:35:41 -0700

    Revert "feat: add AssignmentMenu shared component with cleaner/team/group tabs"
    This reverts commit bfecbe043c943a7d4e4622fcbc3efc6c9a45b92f.

--- src/__tests__/setup/setupTests.ts
+++ src/__tests__/setup/setupTests.ts
@@ -46,11 +46,11 @@ vi.mock('vuetify/lib/components/VAlert/VAlert.css', () => ({}))
 // Global setup
 beforeAll(() => {
   // Mock window properties that aren't available in happy-dom
-  global.ResizeObserver = class ResizeObserver {
-    observe() {}
-    unobserve() {}
-    disconnect() {}
-  }
+  global.ResizeObserver = vi.fn().mockImplementation(() => ({
+    observe: vi.fn(),
+    unobserve: vi.fn(),
+    disconnect: vi.fn(),
+  }))
 
   // Mock matchMedia
   global.matchMedia = vi.fn().mockImplementation(query => ({
